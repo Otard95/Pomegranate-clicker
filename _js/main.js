e592215d4d5f02b0be266dcc2b2f85746366c00c
Stian Myklebostad <stian.myklebostad@getmail.no>
2017-04-12 20:49:23 +0200

    Added server response handlers

--- _js/main.js
+++ _js/main.js
@@ -9,9 +9,52 @@ socket.on('hasPlayer', function(data) {
 });
 
 socket.on('userCreate', function(data) {
-  console.log(data);
+
+  console.log('[userCreate] status: ' + data.status);
+
+  if (data.status) {
+    game.id = data.id;
+    game.me = data.player;
+    var d = new Date();
+    d.setTime(d.getTime() + (30*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = "pId=" + data.id + ";" + expires + ";path=/";
+  } else {
+    // Inform user that the name is taken
+  }
+
 });
 
-socket.on('welcome', function(data) {
-  console.log(data);
+socket.on('buyUpgrade', function(data) {
+
+  console.log('[buyUpgrade] status: ' + data.status);
+
+  if (data.status) {
+    game.me = data.player;
+  } else {
+    // Display fail msg to user
+  }
+
 });
+
+var game = {
+  id: null,
+  me: {
+    seeds: 0,
+    sps: 0,
+    upgrades: {
+      deseeder: {
+        lvl: 0,
+        cost: 0
+      },
+      backyardShrub: {
+        lvl: 0,
+        cost: 0
+      },
+      backyardTree: {
+        lvl: 0,
+        cost: 0
+      }
+    }
+  }
+};
